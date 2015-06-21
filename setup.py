7169bdfb956c6a11db755b626d8361e8c369913b
Алексей Шельф <despawn@gmail.com>
2015-06-21 15:59:46 +0300

    Bump version.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='summarize',
-    version='0.2.1',
+    version='0.2.2',
     description='Simple Python and NLTK-based implementation of text summarization',
     url='https://github.com/despawnerer/summarize',
     author='Aleksei Voronov',
